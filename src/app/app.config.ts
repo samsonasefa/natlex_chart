ebe4b654a4abf291c3490251fb749955b572a473
samsonasefa <somsonasefa@gmail.com>
2024-07-27 11:47:57 +0300

    adding ngrx and ngrx dev-tool

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,17 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideStore, StoreModule } from '@ngrx/store';
+import { environment } from '../environments/environment.development';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync()]
+  providers: [
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    provideStore(),
+    importProvidersFrom(StoreModule.forRoot({})),
+    environment.providers,
+  ],
 };
