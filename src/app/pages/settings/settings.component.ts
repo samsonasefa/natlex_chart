2dcdae95125a4cb53272149e7e4cceaa6272c200
samsonasefa <somsonasefa@gmail.com>
2024-07-29 07:08:20 +0300

    basic menu functions

--- src/app/pages/settings/settings.component.ts
+++ src/app/pages/settings/settings.component.ts
@@ -8,6 +8,8 @@ import { Chart } from '../../model/chart.model';
 import { Store } from '@ngrx/store';
 import { selectCharts } from '../../state/charts.selectors';
 import { CommonModule } from '@angular/common';
+import { ChartsActions } from '../../state/charts.actions';
+import { ChartType } from '../../constants/options';
 
 @Component({
   selector: 'app-settings',
@@ -24,8 +26,72 @@ import { CommonModule } from '@angular/common';
 })
 export class SettingsComponent {
   chartConfigs: Observable<readonly Chart[]>;
+  ChartSamples = ['line', 'candlestick', 'map', 'gantt'];
+  pointer = 0;
+  removedChart = [];
 
   constructor(private store: Store) {
     this.chartConfigs = this.store.select(selectCharts);
   }
+
+  /**
+   * A function to trigger add chart
+   */
+  onAdd(): void {
+    // todo open add modal
+    // on return submit
+    // in the future it will open a modal
+
+    if (this.pointer >= this.ChartSamples.length) {
+      // dont add new chart object if it reach its max
+      return;
+    }
+    this.store.dispatch(
+      ChartsActions.addChart({
+        chart: {
+          id: this.ChartSamples[this.pointer],
+          title: `${this.ChartSamples[this.pointer]} chart`,
+          type: this.ChartSamples[this.pointer] as ChartType,
+          hide: false,
+        },
+      })
+    );
+    // move pointer to next
+    this.pointer = this.pointer + 1;
+  }
+
+  /**
+   * A function responsible to Hide/unHide charts
+   * @param id chart id
+   * @param hide should be visible
+   */
+  changeVisibility(id: string, hide: boolean): void {
+    this.store.dispatch(ChartsActions.hideChart({ id, hide }));
+  }
+
+  /**
+   * A function to trigger edit chart
+   */
+  onEdit(): void {
+    // open edit modal
+    // on return submit / save
+  }
+
+  /**
+   * A function to trigger delete chart
+   */
+  onDelete() {
+    // open 'are you sure' modal
+    // on return save
+  }
+
+  /**
+   * A function responsible to delete chart
+   * @param id chart id to be removed
+   */
+  Remove(id: string): void {
+    this.store.dispatch(ChartsActions.removeChart({ id }));
+  }
+
+  // TODO reset to load default chart
 }
