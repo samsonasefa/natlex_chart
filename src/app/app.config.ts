b628dc3d4a6d54ee5b5fbeb679fb050f8bb0a917
samsonasefa <somsonasefa@gmail.com>
2024-07-31 06:41:56 +0300

    sync between ngrx/store and localstorage

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,10 +3,26 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { provideStore, StoreModule } from '@ngrx/store';
+import {
+  provideStore,
+  ActionReducer,
+  MetaReducer,
+  StoreModule,
+} from '@ngrx/store';
 import { environment } from '../environments/environment.development';
 import { chartsReducer } from './state/charts.reducer';
 import { provideHttpClient } from '@angular/common/http';
+import { localStorageSync } from 'ngrx-store-localstorage';
+
+export function localStorageSyncReducer(
+  reducer: ActionReducer<any>
+): ActionReducer<any> {
+  return localStorageSync({ keys: ['charts'], rehydrate: true })(reducer);
+}
+
+export const metaReducers: Array<MetaReducer<any, any>> = [
+  localStorageSyncReducer,
+];
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -15,9 +31,12 @@ export const appConfig: ApplicationConfig = {
     provideStore(),
     provideHttpClient(),
     importProvidersFrom(
-      StoreModule.forRoot({
-        charts: chartsReducer,
-      })
+      StoreModule.forRoot(
+        {
+          charts: chartsReducer,
+        },
+        { metaReducers }
+      )
     ),
     environment.providers,
   ],
